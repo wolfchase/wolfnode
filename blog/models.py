389ba71e5f7d0d5bbf485a35f64f33a55b5280f0
Rodolfo Castillo <rvallada@ucsc.edu>
2017-12-31 13:45:26 -0800

    Update edit field name in blog post model

--- blog/models.py
+++ blog/models.py
@@ -7,13 +7,16 @@ class Tag(models.Model):
     def __str__(self):
         return self.tag
 
+    class Meta:
+        ordering = ['tag']
+
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     tags = models.ManyToManyField(Tag)
     content = models.TextField(blank=True)
     publish_date = models.DateField()
-    edit_date = models.DateField()
+    edit_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.title
